c1989b9eec9727d393138f0c3251ebed2193c3e7
André Marques <workdreamer@gmail.com>
2011-12-23 12:51:50 +0000

    Fix on scope by city.

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -5,7 +5,7 @@ class Entity < ActiveRecord::Base
     {
         :joins  => "LEFT JOIN cities ON cities.id = entities.city_id",
         :order  => "cities.name DESC",
-        :group  => "entities.id, entities.name, entities.city_id, entities.updated_at,, entities.created_at, cities.name"
+        :group  => "entities.id, entities.name, entities.city_id, entities.updated_at,entities.created_at, cities.name"
         #this group by is needed because i work with MS SQL, and if i didn't perform this i retrieve a error because aggregate function
     }
   }
